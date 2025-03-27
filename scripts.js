a59158f6c3a3a78b82490903ca4583b7ed4b8482
GustavoNadalini <nadalinigustavo@gmail.com>
2025-03-27 11:54:30 -0300

    currency conversion changes

--- scripts.js
+++ scripts.js
@@ -1,30 +1,57 @@
 
 const convertButton = document.querySelector(".convert-button")
 
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues () {
+function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConverted = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
-    
-    const dolarToday = 5.2
 
-    const convertedValue = inputCurrencyValue / dolarToday
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    if (currencySelect.value == 'dolar') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == 'euro') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
 
     currencyValueToConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
     }).format(inputCurrencyValue)
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
+}
 
+function changeCurrency (){
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+    if (currencySelect.value == 'dolar') {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assests/dolar.png"
+
+    }
+
+    if (currencySelect.value == 'euro') {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assests/euro.png"
+    }
+
+    convertValues ()
 }
 
 
-
-
 convertButton.addEventListener("click", convertValues)
+
+currencySelect.addEventListener("change",changeCurrency)
 
